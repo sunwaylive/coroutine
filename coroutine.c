a69aef7b3e4bda19ac3cf66d065de58e23135752
youxijia <sunwayliving@gmail.com>
2017-01-13 15:21:13 +0800

    read and comment the ucontext related codes

--- coroutine.c
+++ coroutine.c
@@ -20,17 +20,17 @@ struct coroutine;
 struct schedule {
 	char stack[STACK_SIZE];
 	ucontext_t main;
-	int nco;
-	int cap;
+	int nco; // number of coroutine
+	int cap; // max coroutine number
 	int running;
 	struct coroutine **co;
 };
 
 struct coroutine {
-	coroutine_func func;
+	coroutine_func func;  // 协程启动函数
 	void *ud;
-	ucontext_t ctx;
-	struct schedule * sch;
+	ucontext_t ctx;       // 协程自己的上下文
+	struct schedule * sch;// 调度器
 	ptrdiff_t cap;
 	ptrdiff_t size;
 	int status;
@@ -81,18 +81,25 @@ coroutine_close(struct schedule *S) {
 	free(S);
 }
 
+// new 一个co， 并放入调度器中
 int 
 coroutine_new(struct schedule *S, coroutine_func func, void *ud) {
 	struct coroutine *co = _co_new(S, func , ud);
+	// 调度器满了之后，2倍扩容
 	if (S->nco >= S->cap) {
 		int id = S->cap;
+
 		S->co = realloc(S->co, S->cap * 2 * sizeof(struct coroutine *));
 		memset(S->co + S->cap , 0 , sizeof(struct coroutine *) * S->cap);
+
+		// 把新的co放到对应位置
 		S->co[S->cap] = co;
 		S->cap *= 2;
+
 		++S->nco;
 		return id;
 	} else {
+
 		int i;
 		for (i=0;i<S->cap;i++) {
 			int id = (i+S->nco) % S->cap;
@@ -107,26 +114,34 @@ coroutine_new(struct schedule *S, coroutine_func func, void *ud) {
 	return -1;
 }
 
+// 传入了一个64bit指针的高32和低32位
 static void
 mainfunc(uint32_t low32, uint32_t hi32) {
 	uintptr_t ptr = (uintptr_t)low32 | ((uintptr_t)hi32 << 32);
+
 	struct schedule *S = (struct schedule *)ptr;
 	int id = S->running;
 	struct coroutine *C = S->co[id];
+
+	// 执行协程work函数
 	C->func(S,C->ud);
+
 	_co_delete(C);
 	S->co[id] = NULL;
 	--S->nco;
 	S->running = -1;
 }
 
+// 恢复id的协程运行
 void 
 coroutine_resume(struct schedule * S, int id) {
 	assert(S->running == -1);
 	assert(id >=0 && id < S->cap);
+
 	struct coroutine *C = S->co[id];
 	if (C == NULL)
 		return;
+
 	int status = C->status;
 	switch(status) {
 	case COROUTINE_READY:
@@ -138,9 +153,13 @@ coroutine_resume(struct schedule * S, int id) {
 		C->status = COROUTINE_RUNNING;
 		uintptr_t ptr = (uintptr_t)S;
 		makecontext(&C->ctx, (void (*)(void)) mainfunc, 2, (uint32_t)ptr, (uint32_t)(ptr>>32));
+		// 保存当前上下文到S->main, 并激活 C->ctx;
+		// 激活之后就会运行C->ctx中的入口函数，就是上面这个mainfunc。因为"C->ctx.uc_link = &S->main;" 后继上下文是S->main
+		// 所以mainfunc运行结束后，会接着swapcontext函数往下执行
 		swapcontext(&S->main, &C->ctx);
 		break;
 	case COROUTINE_SUSPEND:
+		// 在调度器的stack的底部，放置 coroutine的stack
 		memcpy(S->stack + STACK_SIZE - C->size, C->stack, C->size);
 		S->running = id;
 		C->status = COROUTINE_RUNNING;
@@ -155,6 +174,7 @@ static void
 _save_stack(struct coroutine *C, char *top) {
 	char dummy = 0;
 	assert(top - &dummy <= STACK_SIZE);
+
 	if (C->cap < top - &dummy) {
 		free(C->stack);
 		C->cap = top-&dummy;
@@ -164,15 +184,20 @@ _save_stack(struct coroutine *C, char *top) {
 	memcpy(C->stack, &dummy, C->size);
 }
 
+// 手动挂起协程
 void
 coroutine_yield(struct schedule * S) {
 	int id = S->running;
 	assert(id >= 0);
+
 	struct coroutine * C = S->co[id];
 	assert((char *)&C > S->stack);
+
 	_save_stack(C,S->stack + STACK_SIZE);
+
 	C->status = COROUTINE_SUSPEND;
 	S->running = -1;
+
 	swapcontext(&C->ctx , &S->main);
 }
 
